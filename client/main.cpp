05c76c8562f0c1b09567275c5219bb84208935a8
amikellt911 <amikellt911@gmail.com>
2025-09-04 21:01:50 +0800

    唯一实例

--- client/main.cpp
+++ client/main.cpp
@@ -2,9 +2,28 @@
 
 #include <QApplication>
 #include "music.h"
+#include <QLocalServer>
+#include <QLocalSocket>
+#include <QMessageBox>
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    const QString serverName="localUniqueServer";
+    QLocalSocket socket;
+    socket.connectToServer(serverName);
+    if(socket.waitForConnected(500)){
+        QMessageBox::information(nullptr,"提示","程序已经运行");
+        return 0;
+    }
+    QLocalServer server;
+    server.removeServer(serverName);
+    if(!server.listen(serverName))
+    {
+        QMessageBox::critical(nullptr, "错误", "无法启动服务器，程序可能无法正常运行。");
+        return 1;
+    }
+
     //自定义类型必须要注册才能同信号传递
     qRegisterMetaType<Music>("Music"); 
     client w;
